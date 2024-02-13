be0af5a8a019d353ffede3af132ab9f9a7c0823c
Victor Avila <vicav99@gmail.com>
2024-02-12 21:16:19 -0700

    feat: create alert component

---
+++ libs/ui-kit/ui/src/lib/alert/alert.component.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @angular-eslint/no-host-metadata-property */
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
+import { cn } from '@shadcn-ng/ui-kit/util';
+import { cva, VariantProps } from 'class-variance-authority';
+import { ClassValue } from 'clsx';
+
+@Component({
+  standalone: true,
+  selector: 'shadcn-ng-alert',
+  template: `
+    <ng-content select="ng-icon" />
+    <ng-content select="shadcn-ng-alert-title" />
+    <ng-content select="shadcn-ng-alert-description" />
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { '[class]': 'computedClass()' },
+})
+export class AlertComponent {
+  class = input<ClassValue>();
+  variant = input<AlertVariants['variant']>('default');
+
+  protected computedClass = computed(() =>
+    cn(
+      alertVariants({
+        variant: this.variant(),
+      }),
+      this.class()
+    )
+  );
+}
+
+type AlertVariants = VariantProps<typeof alertVariants>;
+
+const alertVariants = cva(
+  'relative flex flex-col w-full rounded-lg border p-4 [&>ng-icon]:absolute [&>ng-icon]:text-foreground [&>ng-icon]:left-4 [&>ng-icon]:top-4 [&>ng-icon+div]:translate-y-[-3px] [&:has(ng-icon)]:pl-11',
+  {
+    variants: {
+      variant: {
+        default: 'bg-background text-foreground',
+        destructive:
+          'border-destructive/50 text-destructive dark:border-destructive [&>ng-icon]:text-destructive',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
