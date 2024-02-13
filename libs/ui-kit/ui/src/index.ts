487c1fe46733a25ec8bf8a8c1f870391b1e46862
Victor Avila <vicav99@gmail.com>
2024-02-12 21:17:01 -0700

    feat: expose alert components

--- libs/ui-kit/ui/src/index.ts
+++ libs/ui-kit/ui/src/index.ts
@@ -1,1 +1,4 @@
+export { AlertComponent } from './lib/alert/alert.component';
+export { AlertDescriptionComponent } from './lib/alert/alert-description/alert-description.component';
+export { AlertTitleComponent } from './lib/alert/alert-title/alert-title.component';
 export { ButtonDirective } from './lib/button/button.directive';
