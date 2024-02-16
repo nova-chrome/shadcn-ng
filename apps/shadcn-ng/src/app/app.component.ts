c57074489eba577f14c112c6fbe0614fc451c7a0
Victor Avila <vicav99@gmail.com>
2024-02-15 18:19:32 -0700

    feat: showcase components in app

--- apps/shadcn-ng/src/app/app.component.ts
+++ apps/shadcn-ng/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {
+  AlertModule,
+  AspectRatioModule,
+  AvatarModule,
+  BadgeModule,
+  ButtonModule,
+} from '@shadcn-ng/ui-kit/ui';
 
 @Component({
   standalone: true,
   selector: 'shadcn-ng-root',
   templateUrl: './app.component.html',
-  imports: [RouterModule],
+  imports: [
+    AlertModule,
+    AspectRatioModule,
+    AvatarModule,
+    BadgeModule,
+    ButtonModule,
+    RouterModule,
+  ],
 })
 export class AppComponent {}
